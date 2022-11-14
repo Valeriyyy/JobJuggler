a1c9ad22fa8deb35ba53d87b2d48fc7ad7d7d18c
Valeriyyy <valeriykutsar18@gmail.com>
2022-11-13 20:21:59 -0800

    Updated app version to dotnet 7

--- Application/DTOs/JobReadDTO.cs
+++ Application/DTOs/JobReadDTO.cs
@@ -1,20 +1,19 @@
 ﻿namespace Application.DTOs;
-public class JobReadDTO
-{
-    public int Id { get; set; }
-    public Guid Guid { get; set; }
-    public decimal Price { get; set; }
-    public string? Notes { get; set; }
-    public bool? IsCompleted { get; set; }
-    public bool? IsCanceled { get; set; }
-    public string? CancelReason { get; set; }
-    public DateTime ScheduledDate { get; set; }
-    public DateTime ScheduledArrivalStartDate { get; set; }
-    public DateTime ScheduledArrivalEndDate { get; set; }
-    public DateTime? StartedDate { get; set; }
-    public DateTime? CompletedDate { get; set; }
-    public DateTime? CanceledDate { get; set; }
-    public ClientDTO Client { get; set; } = null!;
-    public LocationDTO Location { get; set; } = null!;
-    public InvoiceDTO Invoice { get; set; } = null!;
-}
+public record JobReadDTO(
+    int Id,
+    Guid Guid,
+    decimal Price,
+    string? Notes,
+    bool? IsCompleted,
+    bool? IsCanceled,
+    string? CancelReason,
+    DateTime ScheduledDate,
+    DateTime ScheduledArrivalStartDate,
+    DateTime ScheduledArrivalEndDate,
+    DateTime? StartedDate,
+    DateTime? CompletedDate,
+    DateTime? CanceledDate,
+    ClientDTO Client,
+    LocationDTO Location,
+    InvoiceDTO Invoice
+);
