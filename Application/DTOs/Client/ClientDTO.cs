05c1ab858aca0d111fe5efff8fd795b583d38d2a
Valeriyyy <valeriykutsar18@gmail.com>
2023-02-12 13:03:53 -0800

    cleaned up imports

--- Application/DTOs/Client/ClientDTO.cs
+++ Application/DTOs/Client/ClientDTO.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Application.DTOs.Client;
+﻿namespace Application.DTOs.Client;
 public class ClientDTO
 {
     public int Id { get; set; }
