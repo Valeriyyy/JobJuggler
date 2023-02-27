818c607c0afc867a2872a01bce55212ab456155d
Valeriyyy <valeriykutsar18@gmail.com>
2023-02-26 16:12:36 -0800

    formatted class to use new code formatting and new exception constructor

--- Application/Services/ClientService.cs
+++ Application/Services/ClientService.cs
@@ -10,52 +10,42 @@ using Persistence;
 
 namespace Application.Services;
 
-public class ClientService : IClientService
-{
+public class ClientService : IClientService {
     private readonly DataContext _context;
     private readonly IMapper _mapper;
 
-    public ClientService(DataContext context, IMapper mapper)
-    {
+    public ClientService(DataContext context, IMapper mapper) {
         _context = context;
         _mapper = mapper;
     }
 
-    public async Task<List<ClientDTO>> GetClients()
-    {
+    public async Task<List<ClientDTO>> GetClients() {
         var clients = await _context.Clients.ProjectTo<ClientDTO>(_mapper.ConfigurationProvider).ToListAsync();
         return clients;
     }
 
-    public async Task<ClientDTO> CreateClient(ClientInsertDTO clientToInput)
-    {
+    public async Task<ClientDTO> CreateClient(ClientInsertDTO clientToInput) {
         var client = _mapper.Map<Client>(clientToInput);
         _context.Add(client);
 
-        var res = await _context.SaveChangesAsync();
-        Console.WriteLine("This is the save chanes response " + res);
+        await _context.SaveChangesAsync();
         return _mapper.Map<ClientDTO>(client);
     }
 
-    public async Task<ClientDTO?> GetClientById(int clientId)
-    {
+    public async Task<ClientDTO?> GetClientById(int clientId) {
         var client = await _context.Clients.ProjectTo<ClientDTO>(_mapper.ConfigurationProvider).FirstOrDefaultAsync(client => client.Id == clientId);
 
-        if (client is null)
-        {
-            throw new RecordNotFoundException($"No client found with Id {clientId}");
+        if (client is null) {
+            throw new RecordNotFoundException(typeof(Client), clientId);
         }
 
         return client;
     }
 
-    public async Task<ClientDTO?> UpdateClient(int clientId, JsonPatchDocument clientInfo)
-    {
+    public async Task<ClientDTO?> UpdateClient(int clientId, JsonPatchDocument clientInfo) {
         var existingClient = await _context.Clients.FirstOrDefaultAsync(cl => cl.Id == clientId);
-        if (existingClient is null)
-        {
-            var exception = new RecordNotFoundException($"No client found with Id {clientId}");
-            throw exception;
+        if (existingClient is null) {
+            throw new RecordNotFoundException(typeof(Client), clientId);
         }
         clientInfo.ApplyTo(existingClient);
 
