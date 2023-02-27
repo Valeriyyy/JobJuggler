8705c300c65a078cd7f0ff42b4f5e3b19390e426
Valeriyyy <valeriykutsar18@gmail.com>
2023-02-26 16:12:08 -0800

    implemented more constructors and gave class more types for more specific reporting

--- Application/Exceptions/RecordNotFoundException.cs
+++ Application/Exceptions/RecordNotFoundException.cs
@@ -1,5 +1,25 @@
 ﻿namespace Application.Exceptions;
-public class RecordNotFoundException : Exception
-{
+public class RecordNotFoundException : Exception {
+    public Type? ClassType { get; set; }
+    public int? Id { get; set; }
+    public Guid? Guid { get; set; }
+    public RecordNotFoundException() { }
     public RecordNotFoundException(string? message) : base(message) { }
+    public RecordNotFoundException(string message, Exception inner) : base(message, inner) { }
+
+    /// <summary>
+    /// Throws a specific error message notifying that a record of a specific type with a 
+    /// specific id was not found.
+    /// </summary>
+    /// <param name="classType"></param>
+    /// <param name="id"></param>
+    public RecordNotFoundException(Type classType, int id) : base($"No {classType.Name} record found with id {id}") { }
+
+    /// <summary>
+    /// Throws a specific error message notifying that a record of a specific type with a 
+    /// specific id was not found.
+    /// </summary>
+    /// <param name="classType">ClassType</param>
+    /// <param name="guid">Guid</param>
+    public RecordNotFoundException(Type classType, Guid guid) : base($"No {classType.Name} record found with guid {guid}") { }
 }
