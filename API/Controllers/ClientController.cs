f0f1fdfced51d1cc65d476dabbc44531f72e0a31
Valeriyyy <valeriykutsar18@gmail.com>
2023-10-01 17:21:29 -0700

    formatted document

--- API/Controllers/ClientController.cs
+++ API/Controllers/ClientController.cs
@@ -1,5 +1,6 @@
 ﻿using Application.DTOs.Client;
 using Application.Services.Interfaces;
+using Domain.Models;
 using Microsoft.AspNetCore.JsonPatch;
 using Microsoft.AspNetCore.Mvc;
 
@@ -7,42 +8,45 @@ namespace API.Controllers;
 
 [ApiController]
 [Route("api/[controller]")]
-public class ClientController : ControllerBase
-{
+public class ClientController : ControllerBase {
     private readonly IClientService _service;
 
-    public ClientController(IClientService service, ILogger<ClientController> logger)
-    {
+    public ClientController(IClientService service, ILogger<ClientController> logger) {
         _service = service;
     }
 
     [HttpGet(Name = "Get All Clients")]
-    public async Task<ActionResult<IEnumerable<ClientDTO>>> GetAll()
-    {
+    public async Task<ActionResult<IEnumerable<ClientDTO>>> GetAll() {
         return Ok(await _service.GetClients());
     }
 
     [HttpPost(Name = "Create A Single Client")]
-    public async Task<ActionResult<ClientDTO>> Create([FromBody] ClientInsertDTO client)
-    {
+    public async Task<ActionResult<ClientDTO>> Create([FromBody] ClientInsertDTO client) {
         var res = await _service.CreateClient(client);
 
         return res is null ? BadRequest() : Ok(res);
     }
 
     [HttpGet("{id}")]
-    public async Task<ActionResult<ClientDTO>> GetClientById(int id)
-    {
+    public async Task<ActionResult<ClientDTO>> GetClientById(int id) {
         var client = await _service.GetClientById(id);
 
         return client is null ? NotFound() : Ok(client);
     }
 
     [HttpPatch("{id}")]
-    public async Task<ActionResult<ClientDTO>> PatchClientById(int id, [FromBody] JsonPatchDocument client)
-    {
+    public async Task<ActionResult<ClientDTO>> PatchClientById(int id, [FromBody] JsonPatchDocument client) {
         var cl = await _service.UpdateClient(id, client);
 
         return Ok(cl);
+    }
+
+    [HttpGet("profile/{id}")]
+    public async Task<ActionResult<ClientProfile>> GetClientProfile(int clientId) {
+        var profile = await _service.GetProfile(clientId);
+        if (profile is null) {
+            return NotFound();
+        }
+        return Ok(profile);
     }
 }
