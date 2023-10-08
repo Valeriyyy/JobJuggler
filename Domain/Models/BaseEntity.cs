aff1433156c87d0c73088bf572109371eb5854ec
Valeriyyy <valeriykutsar18@gmail.com>
2023-10-07 22:35:57 -0700

    Renamed field names

--- Domain/Models/BaseEntity.cs
+++ Domain/Models/BaseEntity.cs
@@ -1,14 +1,20 @@
-﻿namespace Domain.Models;
+﻿using Domain.IdentityModels;
 
-public abstract class BaseEntity
-{
-    public DateTime? CreatedAt { get; set; }
-    public DateTime? LastModifiedAt{ get; set; }
-    public DateTime? DeletedAt { get; set; }
+namespace Domain.Models;
+
+// This class is meant to be extended by models to put metadata columns on
+// entities. Any business model class should extend this class to provide 
+// the necessary metadata fields.
+public abstract class BaseEntity {
+    public DateTime DateCreated { get; set; }
     public int CreatedById { get; set; }
+    public DateTime DateLastModified { get; set; }
     public int LastModifiedById { get; set; }
-    public int DeletedById { get; set; }
     public bool IsDeleted { get; set; }
+    public DateTime DateDeleted { get; set; }
+    public int DeletedById { get; set; }
+
+
     public virtual AppUser? CreatedBy { get; set; }
     public virtual AppUser? LastModifiedBy { get; set; }
     public virtual AppUser? DeletedBy { get; set; }
