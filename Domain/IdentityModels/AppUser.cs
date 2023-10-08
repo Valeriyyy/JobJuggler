b876e8d7911230585fb0cda80d720bbb0ac9962d
Valeriyyy <valeriykutsar18@gmail.com>
2023-10-07 22:36:29 -0700

    Created directory for identity models

---
+++ Domain/IdentityModels/AppUser.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+#nullable disable
+namespace Domain.IdentityModels;
+
+public class AppUser : IdentityUser<int> {
+    public string DisplayName { get; set; }
+    public int CompanyId { get; set; }
+    public DateTime DateCreated { get; set; }
+    public int CreatedById { get; set; }
+    public DateTime DateLastModified { get; set; }
+    public int LastModifiedById { get; set; }
+    public bool IsDeleted { get; set; }
+    public DateTime DateDeleted { get; set; }
+    public int DeletedById { get; set; }
+}
