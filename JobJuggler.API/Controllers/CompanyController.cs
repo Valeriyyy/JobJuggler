63c0b54f6cae8d9aa1ae922fadce18d904e87595
Valeriyyy <valeriykutsar18@gmail.com>
2023-10-13 21:15:31 -0700

    renamed folders to match project names

---
+++ JobJuggler.API/Controllers/CompanyController.cs
@@ -0,0 +1,23 @@
+﻿using JobJuggler.Domain.IdentityModels;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using JobJuggler.Persistence;
+
+namespace JobJuggler.API.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class CompanyController : ControllerBase {
+    private readonly DataContext _context;
+
+    public CompanyController(DataContext dbContext) {
+        _context = dbContext;
+    }
+
+    [HttpGet]
+    public async Task<ActionResult<AppCompany>> GetCompanies() {
+        var hasComp = await _context.Companies.FirstOrDefaultAsync();
+        Console.WriteLine(hasComp);
+        return Ok(hasComp);
+    }
+}
