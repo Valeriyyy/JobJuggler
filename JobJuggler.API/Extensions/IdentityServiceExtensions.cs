15e76cb32a714142290f0cf13ec3c39dd3c33815
Valeriyyy <valeriykutsar18@gmail.com>
2023-10-23 20:02:02 -0700

    Added authentication extensions

--- JobJuggler.API/Extensions/IdentityServiceExtensions.cs
+++ JobJuggler.API/Extensions/IdentityServiceExtensions.cs
@@ -1,14 +1,31 @@
+using JobJuggler.API.Services;
+using JobJuggler.Domain.IdentityModels;
+using JobJuggler.Persistence;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
+
 namespace JobJuggler.API.Extensions;
 
 public static class IdentityServiceExtensions {
     public static IServiceCollection AddIdentityServices(this IServiceCollection services, IConfiguration config) {
-        /*services.AddIdentityCore<AppUser>(opt =>
-        {
+        services.AddIdentityCore<AppUser>(opt => {
             opt.Password.RequireNonAlphanumeric = false;
+            opt.User.RequireUniqueEmail = false;
         })
         .AddEntityFrameworkStores<DataContext>();
 
-        services.AddAuthentication();*/
+        var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(config["Identity:SecurityTokenKey"]!));
+        services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+            .AddJwtBearer(opt => {
+                opt.TokenValidationParameters = new TokenValidationParameters {
+                    ValidateIssuerSigningKey = true,
+                    IssuerSigningKey = key,
+                    ValidateIssuer = false,
+                    ValidateAudience = false
+                };
+            });
+        services.AddScoped<TokenService>();
 
         return services;
     }
