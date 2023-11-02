055fa8f44fb90aa7bdb0152103e6812da3f31453
Valeriyyy <valeriykutsar18@gmail.com>
2023-11-01 20:10:34 -0700

    Set token to expire in 8 hours

--- JobJuggler.API/Services/TokenService.cs
+++ JobJuggler.API/Services/TokenService.cs
@@ -25,7 +25,7 @@ public class TokenService {
 
         var tokenDescriptor = new SecurityTokenDescriptor {
             Subject = new ClaimsIdentity(claims),
-            Expires = DateTime.UtcNow.AddDays(7),
+            Expires = DateTime.UtcNow.AddHours(8),
             SigningCredentials = creds
         };
 
