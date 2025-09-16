fb6afa13f72da6c32e8216fa6ba0031eb282e774
valeriykutsar18@gmail.com <valeriykutsar18@gmail.com>
2025-09-15 20:52:56 -0700

    added common and test project

---
+++ JobJuggler.Common/Class1.cs
@@ -0,0 +1,12 @@
+﻿using ZLinq;
+
+namespace JobJuggler.Common;
+
+public class Class1
+{
+    public void DoSomething()
+    {
+        var r = Enumerable.Range(1, 10)
+            .Select(x => x * x).AsValueEnumerable().Select(x => x * x);
+    }
+}
